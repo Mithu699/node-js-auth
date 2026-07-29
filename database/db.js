e02dde426e7d3c91fa903fddb54859a422116d15
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-29 21:49:00 +0530

    Add files via upload

---
+++ database/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+  try {
+
+await mongoose.connect(process.env.MONGO_URI)
+console.log('MongoDB Connected successfully ')
+
+
+  } catch (e) {
+    console.error("MongoDB connection failed");
+process.exit(1)
+
+
+  }
+};
+
+module.exports = connectToDB
