617e4b69c3f39d2e109328a83dbeaf1b29a921a0
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-08-05 00:35:36 +0530

    Add files via upload

---
+++ middleware/upload-middleware.js
@@ -0,0 +1,40 @@
+const multer = require("multer");
+const path = require("path");
+
+// set our multer storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+
+  filename: function (req, file, cb) {
+    cb(
+      null,
+
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname),
+    );
+  },
+});
+
+// file filter function
+
+const checkFileFilter = (req, file, cb) => {
+  if (file.mimetype.startWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Not any Image ! please upload only images"));
+  }
+};
+
+
+// multer middleware 
+
+module.exports = multer({
+    storage : storage,
+    fileFilter : checkFileFilter,
+    limits : {
+        fileSize : 5* 1024 *1024 // 5 mb is the size limit
+        
+    }
+})
